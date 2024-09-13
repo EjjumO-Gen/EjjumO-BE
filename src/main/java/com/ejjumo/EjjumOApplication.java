3ce324c112514213dcc8d5dfd0a6dddcfccc36f1
Seungbo Shim <fodo9898@gmail.com>
2024-09-13 16:40:34 +0900

    initial setting

---
+++ src/main/java/com/ejjumo/EjjumOApplication.java
@@ -0,0 +1,13 @@
+package com.ejjumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjjumOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjjumOApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejjumo/EjjumOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejjumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjjumOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
